e06371cd11b5e1e8af2fa4b12c81bf211cfe547b
Alex-maslo <4439897@gmail.com>
2025-02-13 15:22:24 +0200

    getServerSideProps

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -1,16 +1,21 @@
-import React from "react";
 import { getData } from "@/services/api.service";
-import { ICar } from "@/models/ICar";
-import Car from "@/components/Car";
 import Navbar from "@/components/Navbar";
+import Car from "@/components/Car";
+import { ICar } from "@/models/ICar";
 
-const Cars = async () => {
-  const cars: ICar[] = await getData();
+export async function getServerSideProps() {
+  const cars = await getData();
 
+  return {
+    props: { cars },
+  };
+}
+
+const Cars = ({ cars }: { cars: ICar[] }) => {
   return (
     <>
       <Navbar cars={cars} />
-      <div className={`grid sm:grid-cols-5 gap-3 p-3  mt-16 `}>
+      <div className={`grid sm:grid-cols-5 gap-3 p-3 mt-16`}>
         {cars.map((car) => (
           <Car key={car.id} car={car} />
         ))}
