1473964e48990e06888a5f2d309c2283c0f91dfd
Olexandr <4439897@gmail.com>
2025-02-15 15:18:08 +0200

    change code

--- src/components/Cars.tsx
+++ src/components/Cars.tsx
@@ -6,7 +6,7 @@ import { ICar } from "@/models/ICar";
 
 const Cars = async () => {
   const cars: ICar[] = await getData();
-
+  cars.sort((a, b) => b.id - a.id);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4 mt-20">
       <Navbar cars={cars} />
